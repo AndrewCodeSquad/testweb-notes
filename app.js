d01c491532e3b9ceee513c46b02ac10f9dfe7e4a
Andrew O <andrew@codesquad.org>
2019-05-28 15:16:40 -0400

    added views and css directories, style.css and notes.ejs

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app =     express();
+// Storage for notes
+const myNotes = ['HTTP is a protocol', 'Andrew is really cool', 'I like tomatoes'];
+// For /css route, use static files inside CSS folder
+app.use('/css', express.static('css'));
 
-app.get('/', function(req, res) {
-	res.send('Web Notes');
+// render items from array via 'notes' variable on notes.ejs page
+app.get('/', (req, res) => {
+  res.render('notes.ejs', { notes: myNotes });
 });
 
-
-
-
-
-
-app.listen(3000, function() {console.log(`Web Notes app listening on port 3000!`)})
+// Listen on Port 3000
+app.listen(3000, function() {console.log(`Web Notes app listening on port 3000!`)});
